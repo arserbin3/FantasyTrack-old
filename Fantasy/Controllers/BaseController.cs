be2076e6f289b96ab794c9bc49dd20d2acaa1248
Arthur Serbin III <arserbin@vcu.edu>
2013-11-11 19:12:42 -0500

    initial checkin of mvc app

---
+++ Fantasy/Controllers/BaseController.cs
@@ -0,0 +1,73 @@
+﻿namespace Fantasy.Controllers
+{
+    using System.Web.Mvc;
+    using StackExchange.Profiling;
+
+    public class BaseController : Controller
+    {
+        ////private DosDataContext _model;
+        ////private MillenniumDataContext _mill;
+        ////private IntranetDataContext _intranet;
+        private MiniProfiler _profiler;
+
+        ////public DosDataContext DosModel
+        ////{
+        ////    get
+        ////    {
+        ////        return _model ?? (_model = MiniProfilerHelper.Create<DosDataContext>());
+        ////    }
+        ////}
+
+        ////public IntranetDataContext IntranetModel
+        ////{
+        ////    get
+        ////    {
+        ////        return _intranet ?? (_intranet = MiniProfilerHelper.Create<IntranetDataContext>());
+        ////    }
+        ////}
+
+        ////public MillenniumDataContext MillenniumModel
+        ////{
+        ////    get
+        ////    {
+        ////        return _mill ?? (_mill = MiniProfilerHelper.Create<MillenniumDataContext>());
+        ////    }
+        ////}
+
+        protected MiniProfiler Profiler
+        {
+            get
+            {
+                return _profiler ?? (_profiler = MiniProfiler.Current);
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether debug mode is enabled
+        /// </summary>
+        public bool IsDebuggingEnabled
+        {
+            // TODO: consider cacheing this like the other properties
+            get
+            {
+                return ControllerContext.HttpContext.IsDebuggingEnabled;
+            }
+        }
+
+        /// <summary>
+        /// Displays a not authorized message.
+        /// </summary>
+        ////public ViewResult NotAuthorized(string message, string currentTab = null, Form form = null)
+        ////{
+        ////    var viewModel = new BaseViewModel
+        ////    {
+        ////        DisplayMessage = message ?? Constants.NotAuthorized.General,
+        ////        PageTitle = "Not Authorized",
+        ////        CurrentTab = currentTab,
+        ////        Form = form
+        ////    };
+
+        ////    return View("NotAuthorized", viewModel);
+        ////}
+    }
+}
---
+++ Fantasy/ViewModels/BaseViewModel.cs
@@ -0,0 +1,33 @@
+﻿namespace Fantasy.ViewModels
+{
+    public class BaseViewModel
+    {
+        public BaseViewModel()
+        {
+            CurrentTab = "Home";
+        }
+
+        public BaseViewModel(string currentTab)
+        {
+            CurrentTab = currentTab;
+        }
+
+        public string PageTitle { get; set; }
+
+        public string CurrentTab { get; set; }
+
+        public string DisplayMessage { get; set; }
+
+        /// <summary>
+        /// Returns a CSS class for whether the given tab name equals the current tab.
+        /// </summary>
+        /// <param name="tabName">name of the tab to check</param>
+        /// <returns>active if the tab is the current tab, empty string if not.</returns>
+        public string TabClass(string tabName)
+        {
+            return (CurrentTab == tabName
+                ? "active"
+                : "");
+        }
+    }
+}
