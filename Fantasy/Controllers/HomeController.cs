9b9dfc45dd74bcdfe3684a659c7f7dd990d8253e
Arthur Serbin III <arserbin@vcu.edu>
2013-11-11 21:46:49 -0500

    test output from database

--- Fantasy/Controllers/HomeController.cs
+++ Fantasy/Controllers/HomeController.cs
@@ -1,13 +1,18 @@
 ﻿namespace Fantasy.Controllers
 {
+    using System.Linq;
     using System.Web.Mvc;
+    using Models;
     using ViewModels;
 
     public class HomeController : BaseController
     {
         public ActionResult Index()
         {
-            var viewModel = new BaseViewModel();
+            var viewModel = new HomeIndexViewModel
+            {
+                Players = new FantasyContext().Players.ToList()
+            };
 
             return View(viewModel);
         }
---
+++ Fantasy/ViewModels/HomeIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Fantasy.ViewModels
+{
+    using System.Collections.Generic;
+    using Models;
+
+    public class HomeIndexViewModel : BaseViewModel
+    {
+        public HomeIndexViewModel()
+        {
+        }
+
+        public List<Player> Players { get; set; }
+    }
+}
