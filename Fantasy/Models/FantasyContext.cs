27dd307940080bf941b7ea8f1f3ee7b8ca294f70
Arthur Serbin III <arserbin@vcu.edu>
2013-11-11 21:46:30 -0500

    created initial entity framework code-first db

---
+++ Fantasy/Models/FantasyContext.cs
@@ -0,0 +1,20 @@
+﻿namespace Fantasy.Models
+{
+    using System.Data.Entity;
+    using System.Data.Entity.ModelConfiguration.Conventions;
+
+    public class FantasyContext : DbContext
+    {
+        public FantasyContext()
+            : base("FantasyContext")
+        {
+        }
+
+        public DbSet<Player> Players { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ Fantasy/Models/Player.cs
@@ -0,0 +1,16 @@
+﻿namespace Fantasy.Models
+{
+    // TODO: support a player changing teams (or coming from Free Agency)
+    public class Player
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        // TODO: this should be a lookup
+        public string Team { get; set; }
+
+        // TODO: this should be a lookup
+        public string Position { get; set; }
+    }
+}
